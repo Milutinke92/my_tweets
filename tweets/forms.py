0afd8b2af3e9efbfc66a3d96d7bf1b3a3e9eeac4
Stefan Milutinovic <milutinke@scrape.rs>
2016-09-22 09:11:06 +0200

    "112 strana"

--- tweets/forms.py
+++ tweets/forms.py
@@ -7,3 +7,11 @@ class TweetForm(forms.Form):
                                             "cols": 65})
                            )
     country = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+class SearchForm(forms.Form):
+    query = forms.CharField(label="Enter a keyword"
+                                  "to search for",
+                            widget=forms.TextInput(attrs={
+                                "size": 32,
+                                "class": 'form-control'
+                            }))
--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import View
 from django.shortcuts import render
-from .models import HashTag
+from .models import HashTag, Tweet
+from .forms import SearchForm
+import json
 
 class Index(View):
     def get(self, request):
@@ -24,3 +26,31 @@ class HashTagCloud(View):
         hashtag = HashTag.objects.get(name=hashtag)
         params["tweets"] = hashtag.tweet.all()
         return render(request, 'hashtag.html', params)
+
+class Serach(View):
+    """
+    Search all tweets with query /search/?query=<query> URL
+    """
+    def get(self, request):
+        form = SearchForm()
+        params = dict()
+        params["search"] = form
+        return render(request, 'search.html', params)
+
+    def post(self, request):
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            tweets = Tweet.objects.filter(text__icontains=query)
+            context = Context({
+                "query": query,
+                "tweets": tweets
+            })
+            return_str = render_to_string(
+                'partials/_tweet_search.html',
+            context
+            )
+            return HttpResponse(json.dumps(return_str),
+                                content_type="application/json")
+        else:
+            HttpResponseRedirect("/search")
